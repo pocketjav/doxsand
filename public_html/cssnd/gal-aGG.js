3398b12e54ec1ea3e2704b764c160df6d13d1454
Javier <Javier@Javier-PC>
2015-10-18 17:20:43 -0600

    V1.0

---
+++ public_html/cssnd/gal-aGG.js
@@ -0,0 +1,15 @@
+//Elaborado por: Abelardo Gándara González 2015
+function mostrarGaleria(){
+     document.getElementById('tablas').style.display = 'none';
+     document.getElementById('galeria').style.display = 'block';
+     document.getElementById('verGaleria').style.display = 'none';
+     document.getElementById('verTabla').style.display = 'block';
+}
+function ocultarGaleria(){
+     document.getElementById('tablas').style.display = 'block';
+     document.getElementById('galeria').style.display = 'none';
+     document.getElementById('verGaleria').style.display = 'block';
+     document.getElementById('verTabla').style.display = 'none';
+}
+
+
